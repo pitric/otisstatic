286dee8ebe8100529022798d07b5a1f858af8e5e
pitric <writt@pitric.com>
2024-04-26 17:12:17 -0600

    buster-1

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Button, FluentProvider, Text, webLightTheme } from '@fluentui/react-components';
+
 import './App.css'
+import axios from 'axios';
+import React from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [text, setText] = React.useState('before axios...');
+
+  const onApi = async () => {
+
+    const r = await axios.post(`api/play`);
+    setText(r.data);
+  }
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <FluentProvider theme={webLightTheme}>
+      <Text size={900}>12345... {text}</Text>
+      <Button onClick={onApi}>Api...</Button>
+    </FluentProvider>
   )
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), basicSsl()],
+  
+  server: {
+    proxy: {
+      '/api': 'http://127.0.0.1:7071',
+    }, headers: {}
+  },
 })
